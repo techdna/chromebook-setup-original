99a641b52044739f552cf24c3fe353aa1e8329bc
Jan Drake <jan.drake@tech-dna.net>
2022-12-28 09:52:26 -0800

    Launch gnomeboxes

--- chromebook-setup.sh
+++ chromebook-setup.sh
@@ -13,8 +13,11 @@ sudo apt install \
 sudo apt install gnome-boxes -y
 
 # Configure QEMU
-sudo cp /etc/libvirt/qemu.conf /qemu.conf.backup
 sudo sed -i 's/#user/user/g' /etc/libvirt/qemu.conf
 sudo sed -i 's/#group/group/g' /etc/libvirt/qemu.conf
+xhost +
+
+# Start gnome-boxes in background
+ghome-boxes &
 
 #End
