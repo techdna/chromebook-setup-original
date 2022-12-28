25e7c27c7e404cd8bc3c5610b71f03609748afb1
Jan Drake <jan.drake@tech-dna.net>
2022-12-28 09:36:24 -0800

    Initial Version

---
+++ chromebook-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install VM capability (does not support windows, use Parallels)
+sudo apt install \
+	qemu-kvm \
+	libvirt-clients \
+	libvirt-daemon-system \
+	bridge-utils \
+	virtinst \
+	libvirt-daemon \
+	-y
+
+sudo apt install gnome-boxes
+
+# Configure QEMU
+sudo cp /etc/libvirt/qemu.conf /qemu.conf.backup
+sudo sed -i 's/#user/user/g' /etc/libvirt/qemu.conf'
+sudo sed -i 's/#group/group/g' /etc/libvirt/qemu.conf'
+
