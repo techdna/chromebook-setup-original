669cf3beb80629da762b6d6c0c13866a9955ce6d
Jan Drake <jan.drake@tech-dna.net>
2022-12-28 09:45:28 -0800

    Typo fixes to SED command

--- chromebook-setup.sh
+++ chromebook-setup.sh
@@ -10,10 +10,10 @@ sudo apt install \
 	libvirt-daemon \
 	-y
 
-sudo apt install gnome-boxes
+sudo apt install gnome-boxes -y
 
 # Configure QEMU
 sudo cp /etc/libvirt/qemu.conf /qemu.conf.backup
-sudo sed -i 's/#user/user/g' /etc/libvirt/qemu.conf'
-sudo sed -i 's/#group/group/g' /etc/libvirt/qemu.conf'
+sudo sed -i 's/#user/user/g' /etc/libvirt/qemu.conf
+sudo sed -i 's/#group/group/g' /etc/libvirt/qemu.conf
 
