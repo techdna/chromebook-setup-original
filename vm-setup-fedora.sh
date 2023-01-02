e454968ca1102cc32c2e0aec433a73706c0711e5
Jan Drake <jan.drake@tech-dna.net>
2023-01-02 09:01:21 -0800

    Updated vm script

--- vm-setup-fedora.sh
+++ vm-setup-fedora.sh
@@ -27,5 +27,11 @@ sudo sed -i 's/chromium-browser /chromium-browser --disable-features=PreloadMedi
 #docker
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose
 
+# 1Password Command Line
+sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+sudo dnf check-update -y 1password-cli && sudo dnf install 1password-cli
+op --version
+
 #Desktop Config
 gsettings set org.gnome.desktop.background picture-options "scaled"
